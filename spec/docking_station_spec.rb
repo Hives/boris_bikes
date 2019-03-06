17581e049e25ac5e8c75e6e72f2adb7dd2cce617
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-03-06 18:05:09 +0000

    adds initailised default capacity and refactors tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -38,9 +38,15 @@ describe DockingStation do
     expect { @docking_station.release_bike }.to raise_error("No bikes available")
   end
   
-  it "'dock' method raises an error if bike docked when the station is at capacity" do
-    DockingStation::DEFAULT_CAPACITY.times { @docking_station.dock(Bike.new) }
+  it "'dock' method raises an error if bike docked when the station is at DEFAULT_CAPACITY" do
+    @docking_station.capacity.times { @docking_station.dock(Bike.new) }
     expect { @docking_station.dock(@bike1) }.to raise_error("Docking station full")
+  end
+
+  it "'dock' method raises an error if bike docked when the capacity is set other than default" do
+    docking_station = DockingStation.new(108)
+    docking_station.capacity.times { docking_station.dock(Bike.new) }
+    expect { docking_station.dock(@bike1) }.to raise_error("Docking station full")
   end
 
   it "raise error if 'full?' private method called publicly" do
@@ -50,5 +56,16 @@ describe DockingStation do
   it "raise error if 'empty?' private method called publicly" do
     expect { @docking_station.empty? }.to raise_error(NoMethodError)
   end
+
+  it "sets a capacity instance variable when DockingStation.new is called" do
+    docking_station = DockingStation.new(30)
+    expect(docking_station.capacity).to eq 30
+  end
+
+  it "sets a default capacity of DEFAULT_CAPACITY when DockingStation.new is called with no argument" do
+    docking_station = DockingStation.new
+    expect(docking_station.capacity).to eq DockingStation::DEFAULT_CAPACITY
+  end
+
 
 end

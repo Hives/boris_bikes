53d294ad4f7482bf17e4fb4e27adf7cbc342187d
Matt Thompson <matt.thompson@johnlewis.co.uk>
2019-03-06 14:32:16 +0000

    stage 10, passing tests

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -7,11 +7,14 @@ describe DockingStation do
     expect(instance).to respond_to(:release_bike)
   end
 
-  it "will get bike" do
-    # when release_bike is called...
-    # it creates a new Bike instance
-
+  it "will release a bike" do
     docking_station = DockingStation.new
     expect(docking_station.release_bike).to be_an_instance_of(Bike)
+  end
+
+  it "will release a bike that is working" do 
+    docking_station = DockingStation.new
+    bike = docking_station.release_bike
+    expect(bike.working?).to eq true
   end
 end
